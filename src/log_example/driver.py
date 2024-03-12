85b0d0c4526d7b0aca7d464ce4c8fe15fe4f0e94
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-12 10:47:25 +0530

    Added the log_example

--- src/log_example/driver.py
+++ src/log_example/driver.py
@@ -1,1 +1,7 @@
+from util import cal_percent
 
+my_list = [1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 9, 9]
+percentages = cal_percent(my_list)
+
+for key, value in percentages.items():
+    print(f"{key}: {value}%")
--- src/log_example/util.py
+++ src/log_example/util.py
@@ -1,1 +1,16 @@
+def cal_percent(input):
+    element_count = {}
+    for element in input:
+        if element in element_count:
+            element_count[element] += 1
+        else:
+            element_count[element] = 1
+
+    # Calculate percentages
+    total= len(input)
+    percent_dict = {}
+    for key, value in element_count.items():
+        percent_dict[key] = (value / total) * 100
+
+    return percent_dict
 
